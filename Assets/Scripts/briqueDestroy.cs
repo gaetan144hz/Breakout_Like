7594d1c0d382b94dcc65c440b1d39fff44c4b336
gaetan144hz <g.dumas@etudiant.ecolemelies.fr>
2022-02-11 01:32:19 +0100

    sound

--- Assets/Scripts/briqueDestroy.cs
+++ Assets/Scripts/briqueDestroy.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AudioSource))]
+
 public class briqueDestroy : MonoBehaviour
 {
     [SerializeField] private GameObject deathEffect;
@@ -14,8 +17,13 @@ public class briqueDestroy : MonoBehaviour
     [SerializeField] private GameObject DeathEffectPrefabs;
     
     private Rigidbody2D rb;
+
+    private AudioSource audioSource;
+    
     void Awake()
     {
+        audioSource = this.GetComponent<AudioSource>();
+        
         rb = this.GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
     }
@@ -27,6 +35,8 @@ public class briqueDestroy : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D col)
     {
+        audioSource.Play();
+        
         if (col.gameObject.CompareTag("Balle"))
         {
             Die();
